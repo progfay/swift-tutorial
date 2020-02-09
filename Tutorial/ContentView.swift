32e5c9a66da68ed30b5e92445b5b58afaa9672ae
progfay <shun1856shun@gmail.com>
2020-02-09 17:59:16 +0900

    Initial Commit

---
+++ Tutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tutorial
+//
+//  Created by 眞野隼輔 on 2020/02/09.
+//  Copyright © 2020 progfay. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TutorialTests/TutorialTests.swift
@@ -0,0 +1,34 @@
+//
+//  TutorialTests.swift
+//  TutorialTests
+//
+//  Created by 眞野隼輔 on 2020/02/09.
+//  Copyright © 2020 progfay. All rights reserved.
+//
+
+import XCTest
+@testable import Tutorial
+
+class TutorialTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
